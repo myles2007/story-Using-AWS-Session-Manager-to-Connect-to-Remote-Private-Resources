c0e363a8de87f10116a3862758a8de1501d165c7
Myles Loffler <myles2007@gmail.com>
2024-03-16 21:42:48 -0500

    init: bootstrap the repo

---
+++ README.md
@@ -0,0 +1,36 @@
+# Welcome
+
+This project is intended to be used alongside my series of Medium stories under
+umbrella title:
+
+"Using AWS Session Manager to Connec to Remote, Private Resources"
+
+At this time, it covers:
+
+ - Connecting to a private EC2 instance using AWS Session Manager
+ - (Coming Soon) Leveraging Session Manager Port Forwarding to connect to a private database
+
+The project structure is reflective of `cdk init app --languge=typescript` and is comprised
+of several stacks to allow for modular deployment.
+
+The stacks are:
+
+ - Networking (required for all stories)
+ - BasicCompute
+ - AuroraDB
+
+Each story will explain specifically which stacks are necessary
+
+>[!WARNING]
+> Several of the resources launched by this example (e.g., the EC2 instance and
+> Aurora RDS Cluster) are billed based on the time they are provisined.
+> Free Tier eligible resources are used wherever possible, but none of these
+> resources exist in the Always Free Tier and will eventually become paid.
+>
+> **You should delete your CloudFormation stacks associated with this repository
+> when you are done with them to avoid unexpected bills**
+
+Please see the following Medium stories as guides for using this repository:
+
+ - (Coming soon)
+ - (Coming soon)
---
+++ bin/using-session-manager.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { NetworkStack } from '../lib/stack.Networking';
+import { BastionStack } from '../lib/stack.BastionHost';
+import { AuroraDBStack } from '../lib/stack.AuroraDB';
+
+const app = new cdk.App();
+new NetworkStack(app, 'UsingSessionManager-Network', {
+  /* If you don't specify 'env', this stack will be environment-agnostic.
+   * Account/Region-dependent features and context lookups will not work,
+   * but a single synthesized template can be deployed anywhere. */
+
+  /* Uncomment the next line to specialize this stack for the AWS Account
+   * and Region that are implied by the current CLI configuration.
+   * If you do this, add similar declarations to each stack in the app.
+  */
+  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+
+  /* Uncomment the next line if you know exactly what Account and Region you
+   * want to deploy the stack to. */
+  // env: { account: '123456789012', region: 'us-east-1' },
+
+  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+});
+new BastionStack(app, 'UsingSessionManager-Bastion', {})
+new AuroraDBStack(app, 'UsingSessionManager-AuroraDB', {})
